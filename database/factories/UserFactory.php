684326ced445b348f30e1cd7f16afc948ba2845d
Yudi Andela <yudhi.andhela@gmail.com>
2018-12-03 07:33:29 +0700

    Add user seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,12 +13,12 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Models\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name'              => 'Super Admin',
+        'email'             => 'admin@example.com',
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'password'          => '$2y$10$NQUIWcXCKAkywZrEh1YE4.jyAVgTWsmg4/lIAuu94oCoGXrgkByaC', // 123456
+        'remember_token'    => str_random(10),
     ];
 });
