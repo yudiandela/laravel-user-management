3ba6e6a635fe2e1b7f82807c0751024b780b0a69
Yudi Andela <yudhi.andhela@gmail.com>
2018-12-07 23:09:08 +0700

    Install dan konfigurasi laravel/browser-kit-testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,15 +7,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    /** @test */
+    public function BasicTest()
     {
-        $response = $this->get('/');
+        $this->get('/');
 
-        $response->assertStatus(200);
+        $this->seeText('Laravel');
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -7,12 +7,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    /** @test */
+    public function BasicTest()
     {
         $this->assertTrue(true);
     }
