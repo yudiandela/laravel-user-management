036e10a23707cf3129b4d34167d83ff27391174d
Yudi Andela <yudhi.andhela@gmail.com>
2018-12-09 09:42:33 +0700

    merapikan view create user

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -13,8 +13,8 @@
 
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Dashboard</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Update User</li>
+                <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">{{ __('Dashboard') }}</a></li>
+                <li class="breadcrumb-item active" aria-current="page">{{ __('Update User') }}</li>
             </ol>
         </nav>
     </div>
@@ -27,7 +27,7 @@
                         @csrf
                         @method('PUT')
                         <div class="form-group row">
-                            <label for="name" class="col-sm-3 col-form-label">Full Name</label>
+                            <label for="name" class="col-sm-3 col-form-label">{{ __('Full Name') }}</label>
                             <div class="col-sm-6">
                                 <input name="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" value="{{ old('name') ? old('name') : $user->name }}" id="name" placeholder="Full Name">
                                 @if ($errors->has('name'))
@@ -39,7 +39,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-3 col-form-label">Email Address</label>
+                            <label for="email" class="col-sm-3 col-form-label">{{ __('Email Address') }}</label>
                             <div class="col-sm-6">
                                 <input name="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" value="{{ old('email') ? old('email') : $user->email }}" id="email" placeholder="Email Address">
                                 @if ($errors->has('email'))
@@ -51,7 +51,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="username" class="col-sm-3 col-form-label">Username</label>
+                            <label for="username" class="col-sm-3 col-form-label">{{ __('Username') }}</label>
                             <div class="col-sm-6">
                                 <input name="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" value="{{ old('username') ? old('username') : $user->username }}" id="username" placeholder="Username">
                                 @if ($errors->has('username'))
@@ -63,12 +63,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="name" class="col-sm-3 col-form-label">Role</label>
+                            <label for="name" class="col-sm-3 col-form-label">{{ __('Role') }}</label>
                             <div class="col-sm-2">
                                 <div class="form-check">
                                     <label class="form-check-label">
                                         <input type="radio" class="form-check-input" name="role" id="role2" value="2" @if ($user->role == 2) {{ 'checked=""' }}@endif>
-                                        Admin
+                                        {{ __('Admin') }}
                                         <i class="input-helper"></i>
                                     </label>
                                 </div>
@@ -77,7 +77,7 @@
                                 <div class="form-check">
                                     <label class="form-check-label">
                                         <input type="radio" class="form-check-input" name="role" id="role3" value="3" @if ($user->role == 3) {{ 'checked=""' }}@endif>
-                                        Staf
+                                        {{ __('Staf') }}
                                         <i class="input-helper"></i>
                                     </label>
                                 </div>
@@ -86,7 +86,7 @@
                                 <div class="form-check">
                                     <label class="form-check-label">
                                         <input type="radio" class="form-check-input" name="role" id="role99" value="99" @if ($user->role == 99) {{ 'checked=""' }}@endif>
-                                        Member
+                                        {{ __('Member') }}
                                         <i class="input-helper"></i>
                                     </label>
                                 </div>
@@ -94,13 +94,13 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="photo" class="col-sm-3 col-form-label">Photo</label>
+                            <label for="photo" class="col-sm-3 col-form-label">{{ __('Photo') }}</label>
                             <div class="col-sm-6">
                                 <input type="file" name="photo" class="file-upload-default" value="{{ old('photo') ? old('photo') : $user->photo }}">
                                 <div class="input-group col-xs-12">
                                     <input type="text" class="form-control file-upload-info{{ $errors->has('photo') ? ' is-invalid' : '' }}" value="{{ old('photo') ? old('photo') : $user->photo }}" disabled="" placeholder="Upload Image">
                                     <span class="input-group-append">
-                                        <button class="file-upload-browse btn btn-gradient-primary" type="button">Image</button>
+                                        <button class="file-upload-browse btn btn-gradient-primary" type="button">{{ __('Image') }}</button>
                                     </span>
                                     @if ($errors->has('photo'))
                                         <div class="invalid-feedback">
@@ -114,8 +114,8 @@
                         <div class="row">
                             <div class="col-sm-3"></div>
                             <div class="col-sm-6">
-                                <button type="submit" class="btn btn-gradient-primary mr-2">Submit</button>
-                                <button class="btn btn-light">Cancel</button>
+                                <button type="submit" class="btn btn-gradient-primary mr-2">{{ __('Submit') }}</button>
+                                <a href="{{ route('admin.user.index') }}" class="btn btn-light">{{ __('Cancel') }}</a>
                             </div>
                         </div>
 
